ae26a3a326b5cf6e5ea291d52a1eedf6ecb448cf
Melker Liljegren <melkerliljegren@hotmail.se>
2025-10-16 23:46:09 +0200

    Completed part 2 of day 5

--- day05/part2.py
+++ day05/part2.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from functools import cmp_to_key
 
 with open("day05/input.txt", "r", encoding="utf-8") as f:
     rules_text, updates_text = f.read().strip().split("\n\n")
@@ -7,7 +7,28 @@ with open("day05/input.txt", "r", encoding="utf-8") as f:
 rules = [tuple(map(int, r.split("|"))) for r in rules_text.splitlines()]
 updates = [list(map(int, u.split(","))) for u in updates_text.splitlines()]
 
-"""Creating a dictionary that showcase which updates needs to be after another update"""
-after = defaultdict(set)
-for x, y in rules:
-    after[x].add(y)
+"""Creating a dictionary with all rules"""
+after = set(rules)
+
+"""Defining the key for the sorted function"""
+
+
+def cmp(x, y):
+    if (x, y) in after:
+        return -1
+    elif (y, x) in after:
+        return 1
+    return 0
+
+
+"""
+Looping through the updates and sorting the incorrectly-ordered update
+cmp_to_key makes it possible for sorted to sort after our rules
+"""
+total = 0
+for upd in updates:
+    fixed = sorted(upd, key=cmp_to_key(cmp))
+    if fixed != upd:
+        total += fixed[len(fixed) // 2]
+
+print(total)
