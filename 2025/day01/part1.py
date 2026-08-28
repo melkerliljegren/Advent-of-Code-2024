7846f5c667b20a695be0df286e875473001a38aa
Melker Liljegren <melkerliljegren@hotmail.se>
2026-08-28 11:18:55 +0200

    part1 completed

---
+++ 2025/day01/part1.py
@@ -0,0 +1,13 @@
+with open("2025/day01/input.txt", "r", encoding="utf-8") as f:
+    document = [(s[0], int(s[1:])) for s in (line.strip() for line in f)]
+
+counter = 0
+count = 50
+
+for d, c in document:
+    steps = c if d == "R" else -c
+    count = (count + steps) % 100
+    if count == 0:
+        counter += 1
+
+print(counter)
